f788bc5b514ea759b51c92a4b4ae1de83afc3744
CengizhanOemuer <cengiz.cmr@gmail.com>
2023-09-20 18:18:10 +0200

    Added a basic window

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        launch(args);
+    }
 
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Window settings:
+        primaryStage.setTitle("Password-Manager");
 
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
-            System.out.println("i = " + i);
-        }
+        //
+        StackPane root = new StackPane();
+        root.getChildren().addAll();
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
