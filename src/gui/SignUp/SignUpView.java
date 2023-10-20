ba8ba7213709f98f89136aa0d77783a11c706216
_c3ngiz_ <cengiz.cmr@gmail.com>
2023-10-20 20:35:56 +0200

    Started working on the sign-up view

--- src/gui/SignUp/SignUpView.java
+++ src/gui/SignUp/SignUpView.java
@@ -1,15 +1,26 @@
 package gui.SignUp;
 
 import business.SignUp.SignUpModel;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class SignUpView {
     // Attributes:
     private SignUpModel signUpModel;
     private SignUpControl signUpControl;
+
     // Constructors:
     public SignUpView(SignUpControl signUpControl, Stage primaryStage, SignUpModel signUpModel) {
         this.signUpModel = signUpModel;
@@ -26,18 +37,63 @@ public class SignUpView {
         this.initComponents();
         this.initListener();
     }
+
     // GUI-Attributes:
-    private Pane root = new Pane();
-    private final int SCENE_HEIGHT = 340;
-    private final int SCENE_WIDTH = 560;
-    private Label lblTest = new Label("Test");
+    /* ---------------------------------------------- */
+    /* Window */
+    private final Pane root = new Pane();
+    private final double SCENE_HEIGHT = 340;
+    private final double SCENE_WIDTH = 560;
+    /* Window */
+    /* ---------------------------------------------- */
+    /* Fonts */
+    private final Font TITLE_FONT = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 20.0);
+    /* Fonts */
+    /* ---------------------------------------------- */
+    /* Labels for V-Box */
+    private Label lblSignUp;
+    /* Labels for V-Box */
+    /* ---------------------------------------------- */
+    /* Text-fields for V-Box */
+    private TextField txtFieldInputUsername;
+    private TextField txtFieldInputPassword;
+    private TextField txtFieldInputPasswordAgain;
+    /* Text-fields for V-Box */
+    /* ---------------------------------------------- */
+    /* -V-Box- */
+    private VBox vBoxSignUp;
+    /* -V-Box- */
+    /* ---------------------------------------------- */
 
     // Initialising-Components-Method:
     private void initComponents() {
-        // Add components here:
-
+        // V-Box:
+        vBoxSignUp = new VBox();
+        vBoxSignUp.setBackground(Background.fill(Color.WHITE));
+        vBoxSignUp.setBorder(Border.stroke(Color.BLACK));
+        vBoxSignUp.setSpacing(10);
+        vBoxSignUp.setPadding(new Insets(10, 15, 10, 15));
+        vBoxSignUp.setPrefWidth(SCENE_WIDTH - 300);
+        vBoxSignUp.setPrefHeight(SCENE_HEIGHT - 75);
+        vBoxSignUp.setLayoutX(150);
+        vBoxSignUp.setLayoutY(37);
+        // Label:
+        lblSignUp = new Label("Sign Up");
+        lblSignUp.setPadding(new Insets(10, 75, 10, 75));
+        lblSignUp.setFont(TITLE_FONT);
+        // Text-fields:
+        txtFieldInputUsername = new TextField();
+        txtFieldInputPassword = new TextField();
+        txtFieldInputPasswordAgain = new TextField();
+        txtFieldInputUsername.setPromptText("Username");
+        txtFieldInputUsername.setMaxWidth(230);
+        txtFieldInputPassword.setPromptText("Password");
+        txtFieldInputPassword.setMaxWidth(230);
+        txtFieldInputPasswordAgain.setPromptText("Repeat password");
+        txtFieldInputPasswordAgain.setMaxWidth(230);
         // ---
-        root.getChildren().addAll(lblTest);
+        vBoxSignUp.getChildren().addAll(lblSignUp, txtFieldInputUsername, txtFieldInputPassword, txtFieldInputPasswordAgain);
+        root.getChildren().addAll(vBoxSignUp);
     }
 
     // Initialising-Listeners-Method:
