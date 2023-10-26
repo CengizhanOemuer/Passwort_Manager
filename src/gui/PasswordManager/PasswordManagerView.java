c96cfcbd26196be599d98c4133b5051a9fe4f388
_c3ngiz_ <cengiz.cmr@gmail.com>
2023-10-26 22:22:43 +0200

    Started refactoring PasswordManagerView

--- src/gui/PasswordManager/PasswordManagerView.java
+++ src/gui/PasswordManager/PasswordManagerView.java
@@ -1,33 +1,30 @@
 package gui.PasswordManager;
 
 import business.PasswordManager.PasswordManagerModel;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class PasswordManagerView {
-    // MVC-Pattern Attributes:
-    private PasswordManagerModel passwordManagerModel;
-    private PasswordManagerControl passwordManagerControl;
+    // Attributes:
+    private final PasswordManagerModel passwordManagerModel;
+    private final PasswordManagerControl passwordManagerControl;
 
-    // MVC-Pattern Constructor:
+    // Constructor:
     public PasswordManagerView(PasswordManagerControl passwordManagerControl, Stage primaryStage, PasswordManagerModel passwordManagerModel) {
         this.passwordManagerModel = passwordManagerModel;
         this.passwordManagerControl = passwordManagerControl;
+        this.primaryStage = primaryStage;
 
         // Initialising the window:
-        Scene scene = new Scene(this.pane, sceneWidth, sceneHeight);
+        Scene scene = new Scene(this.root, SCENE_WIDTH, SCENE_HEIGHT);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Password-Manager");
@@ -38,148 +35,174 @@ public class PasswordManagerView {
     }
 
     // GUI-Attributes:
+    /* ---------------------------------------------- */
+    /* Window */
+    private final Stage primaryStage;
+    private final Pane root = new Pane();
+    private final double SCENE_HEIGHT = 600;
+    private final double SCENE_WIDTH = 800;
 
-    // ---------------------- window, scene & fonts ---------------------- //
-    private Pane pane = new Pane();
-    private double sceneHeight = 600;
-    private double sceneWidth = 800;
-    private Font titleFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 24);
-    private Font textFont = Font.font("Arial", FontWeight.NORMAL, 14);
-    // ---------------------- window, scene & fonts ---------------------- //
+    /* Window */
+    /* ---------------------------------------------- */
+    /* Fonts */
+    private final Font TITLE_FONT = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 24);
+    private final Font LABEL_FONT = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 20);
+    private final Font TEXT_FONT = Font.font("Arial", FontWeight.NORMAL, 14);
+    /* Fonts */
+    /* ---------------------------------------------- */
+    /* Password-Generator-Attributes */
+    /* ---------------------------------------------- */
+    /* Labels for vBox of Password-Generator */
+    private Label lblPasswordGenerator;
+    private Label lblLengthOfPassword;
+    private Label lblLengthNumber;
+    /* Labels for vBox of Password-Generator */
+    /* ---------------------------------------------- */
+    /* Text Areas for vBox of Password-Generator */
+    private TextArea txtAreaPassword;
+    /* Text Areas for vBox of Password-Generator */
+    /* ---------------------------------------------- */
+    /* Text fields for vBox of Password-Generator */
+    private TextField txtFieldWebsite;
+    private TextField txtFieldUsername;
+    /* Text fields for vBox of Password-Generator */
+    /* ---------------------------------------------- */
+    /* Checkboxes for Password-Generator */
+    private CheckBox checkBoxUpper;
+    private CheckBox checkBoxLower;
+    private CheckBox checkBoxNumbers;
+    private CheckBox checkBoxSpecials;
 
-    // ---------------------- standard elements ---------------------- //
-    private Text txtQuestion = new Text("What do u want to do?");
-    private Text txtPasswordCreation = new Text("Create your Password");
-    private RadioButton rBtnCreateNewPassword = new RadioButton("Create new Password!");
-    private RadioButton rBtnLoadAllSavedPasswords = new RadioButton("Load passwords!");
-    ToggleGroup toggleGroup = new ToggleGroup();
-    private VBox vBox = new VBox(rBtnCreateNewPassword, rBtnLoadAllSavedPasswords);
+    /* Checkboxes for Password-Generator */
+    /* ---------------------------------------------- */
+    /* Buttons for Password-Generator */
+    private Button btnGenerate;
+    private Button btnSave;
+    /* Buttons for Password-Generator */
+    /* ---------------------------------------------- */
+    /* Slider for Password-Generator */
+    private Slider sliderForLength;
+    /* Slider for Password-Generator */
+    /* ---------------------------------------------- */
+    /* HBoxes for Password-Generator */
+    private HBox hBoxSliderLabels;
+    /* HBoxes for Password-Generator */
+    /* ---------------------------------------------- */
+    /* VBoxes for Password-Generator */
+    private VBox vBoxPasswordGenerator;
+    private VBox vBoxSettingsComponents;
+    private VBox vBoxCheckboxes;
+    /* VBox for Password-Generator */
+    /* ---------------------------------------------- */
+    /* Password-Generator-Attributes */
+    /* ---------------------------------------------- */
+    // GUI-Attributes:
+
     private TableView tableView = new TableView();
     private TableColumn<String, String> columnWebsite = new TableColumn<>("Website");
     private TableColumn<String, String> columnUsername = new TableColumn<>("Username");
     private TableColumn<String, String> columnPassword = new TableColumn<>("Password");
-    // ---------------------- standard elements ---------------------- //
-
-    // ---------------------- create new password  ---------------------- //
-    private Text txtLength = new Text("Length of password");
-    private Text txtUserinformation = new Text();
-
-    private TextField txtFieldWebsite = new TextField();
-    private TextField txtFieldUsername = new TextField();
-    private TextField txtFieldLength = new TextField();
-    private TextField txtFieldGeneratedPassword = new TextField("");
-
-    private CheckBox checkBoxIncludeUpper = new CheckBox("Include uppercase letters");
-    private CheckBox checkBoxIncludeLower = new CheckBox("Include lowercase letters");
-    private CheckBox checkBoxIncludeNumbers = new CheckBox("Include numbers");
-    private CheckBox checkBoxIncludeSpecialCharacters = new CheckBox("Include special characters");
-    private Button btnCreatePassword = new Button("Create password!");
-    private Button btnSavePassword = new Button("Save");
-    HBox hBoxInputLength = new HBox(txtLength, txtFieldLength);
-    HBox hboxBtns = new HBox(btnCreatePassword, btnSavePassword);
-    VBox vBoxForNewPassword = new VBox(txtPasswordCreation, txtFieldWebsite, txtFieldUsername, hBoxInputLength, checkBoxIncludeUpper, checkBoxIncludeLower, checkBoxIncludeNumbers, checkBoxIncludeSpecialCharacters, txtFieldGeneratedPassword, hboxBtns, txtUserinformation);
-    // ---------------------- create new password  ---------------------- //
 
     // Methods:
     private void initComponents() {
-        // Labels:
-        txtQuestion.setLayoutX(60);
-        txtQuestion.setLayoutY(60);
-        txtQuestion.setFont(titleFont);
-        pane.getChildren().addAll(txtQuestion);
+        // V-Boxes for Password-Generator:
+        vBoxPasswordGenerator = new VBox();
+        vBoxPasswordGenerator.setBackground(Background.fill(Color.WHITE));
+        vBoxPasswordGenerator.setBorder(Border.stroke(Color.BLACK));
+        vBoxPasswordGenerator.setSpacing(10);
+        vBoxPasswordGenerator.setPadding(new Insets(10, 15, 10, 15));
+        vBoxPasswordGenerator.setPrefWidth(SCENE_WIDTH/2 - 10);
+        vBoxPasswordGenerator.setPrefHeight(SCENE_HEIGHT-10);
+        vBoxPasswordGenerator.setLayoutX(5);
+        vBoxPasswordGenerator.setLayoutY(5);
 
-        // Radio-Buttons 1:
-        rBtnCreateNewPassword.setToggleGroup(toggleGroup);
-        rBtnLoadAllSavedPasswords.setToggleGroup(toggleGroup);
-        rBtnCreateNewPassword.setFont(textFont);
-        rBtnLoadAllSavedPasswords.setFont(textFont);
+        vBoxSettingsComponents = new VBox();
+        vBoxSettingsComponents.setSpacing(10);
 
-        // VBoxes:
-        vBox.setLayoutX(70);
-        vBox.setLayoutY(80);
-        vBox.setSpacing(10);
-        pane.getChildren().addAll(vBox);
+        vBoxCheckboxes = new VBox();
+        vBoxCheckboxes.setSpacing(5);
 
+        // HBoxes for Password-Generator:
+        hBoxSliderLabels = new HBox();
+        hBoxSliderLabels.setSpacing(25);
 
+        // Labels for Password-Generator:
+        lblPasswordGenerator = new Label("Password Generator");
+        lblPasswordGenerator.setPadding(new Insets(0, 60, 0, 60));
+        lblPasswordGenerator.setFont(TITLE_FONT);
+
+        lblLengthOfPassword = new Label("Length of Password:");
+        lblLengthOfPassword.setFont(TEXT_FONT);
+
+        lblLengthNumber = new Label("8");
+        lblLengthNumber.setFont(TEXT_FONT);
+
+        // Text Area for Password-Generator:
+        txtAreaPassword = new TextArea();
+        txtAreaPassword.setMaxHeight(20);
+        txtAreaPassword.setEditable(false);
+        txtAreaPassword.setPromptText("Pa$$w0rd");
+
+        // Slider for Password-Generator:
+        sliderForLength = new Slider(8, 32, 8);
+        sliderForLength.setMajorTickUnit(1);
+        sliderForLength.setShowTickMarks(true);
+        sliderForLength.setShowTickLabels(true);
+        sliderForLength.setSnapToTicks(true);
+
+        // Checkboxes for Password-Generator:
+        checkBoxUpper = new CheckBox("Include upper");
+        checkBoxLower = new CheckBox("Include lower");
+        checkBoxNumbers = new CheckBox("Include numbers");
+        checkBoxSpecials = new CheckBox("Include specials");
+
+        checkBoxUpper.setFont(TEXT_FONT);
+        checkBoxLower.setFont(TEXT_FONT);
+        checkBoxNumbers.setFont(TEXT_FONT);
+        checkBoxSpecials.setFont(TEXT_FONT);
+
+        // Text-fields for Password-Generator:
+        txtFieldWebsite = new TextField();
+        txtFieldUsername = new TextField();
+
+        txtFieldWebsite.setPromptText("Website");
+        txtFieldUsername.setPromptText("Username");
+
+        // Buttons for Password-Generator:
+        btnGenerate = new Button("Generate");
+        btnSave = new Button("Save");
+
+        /*
         // Columns:
         columnWebsite.setCellValueFactory(new PropertyValueFactory<>("Website"));
-        columnWebsite.setPrefWidth((sceneWidth / 2) / 3 - 5);
+        columnWebsite.setPrefWidth((SCENE_WIDTH / 2) / 3 - 5);
         columnUsername.setCellValueFactory(new PropertyValueFactory<>("Username"));
-        columnUsername.setPrefWidth((sceneWidth / 2) / 3 - 5);
+        columnUsername.setPrefWidth((SCENE_WIDTH / 2) / 3 - 5);
         columnPassword.setCellValueFactory(new PropertyValueFactory<>("Password"));
-        columnPassword.setPrefWidth((sceneWidth / 2) / 3 - 5);
+        columnPassword.setPrefWidth((SCENE_WIDTH / 2) / 3 - 5);
+
 
         // TableView:
         tableView.getColumns().addAll(columnWebsite, columnUsername, columnPassword);
-        tableView.setLayoutX(sceneWidth / 2);
+        tableView.setLayoutX(SCENE_WIDTH / 2);
         tableView.setLayoutY(30);
-        tableView.setPrefHeight(sceneHeight - 60);
-        tableView.setPrefWidth(sceneWidth / 2 - 30);
-        pane.getChildren().addAll(tableView);
+        tableView.setPrefHeight(SCENE_HEIGHT - 60);
+        tableView.setPrefWidth(SCENE_WIDTH / 2 - 30);
+         */
+
+        // ---
+        vBoxCheckboxes.getChildren().addAll(checkBoxUpper, checkBoxLower, checkBoxNumbers, checkBoxSpecials);
+        hBoxSliderLabels.getChildren().addAll(lblLengthOfPassword, lblLengthNumber);
+
+        vBoxSettingsComponents.getChildren().addAll(hBoxSliderLabels, sliderForLength, vBoxCheckboxes, btnGenerate, txtFieldWebsite, txtFieldUsername, btnSave);
+
+        vBoxPasswordGenerator.getChildren().addAll(lblPasswordGenerator, txtAreaPassword, vBoxSettingsComponents);
+
+        root.getChildren().addAll(vBoxPasswordGenerator);
     }
 
     private void initListener() {
-        rBtnCreateNewPassword.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                // Initialising texts:
-                txtPasswordCreation.setFont(titleFont);
-                txtLength.setFont(textFont);
-                txtUserinformation.setFont(textFont);
-                txtUserinformation.setFill(Color.RED);
-                // Initialising text-fields for input:
-                txtFieldWebsite.setPromptText("Website");
-                txtFieldWebsite.setMaxWidth(225);
-                txtFieldUsername.setPromptText("Username");
-                txtFieldUsername.setMaxWidth(225);
-                txtFieldLength.setPromptText("Integer");
-                txtFieldLength.setMaxWidth(60);
-                // Initialising text-fields for output:
-                txtFieldGeneratedPassword.setEditable(false);
-                // Initialising checkboxes:
-                checkBoxIncludeUpper.setFont(textFont);
-                checkBoxIncludeLower.setFont(textFont);
-                checkBoxIncludeNumbers.setFont(textFont);
-                checkBoxIncludeSpecialCharacters.setFont(textFont);
-                // Initialising buttons:
-                btnCreatePassword.setFont(textFont);
-                btnSavePassword.setFont(textFont);
-                btnSavePassword.setPrefWidth(130);
-                // Initialising hBoxes:
-                hBoxInputLength.setSpacing(5);
-                hboxBtns.setSpacing(5);
-                // Initialising vBox:
-                vBoxForNewPassword.setLayoutX(60);
-                vBoxForNewPassword.setLayoutY(140);
-                vBoxForNewPassword.setSpacing(10);
 
-                pane.getChildren().addAll(vBoxForNewPassword);
-            }
-        });
-        rBtnLoadAllSavedPasswords.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                pane.getChildren().removeAll(vBoxForNewPassword);
-            }
-        });
-        btnCreatePassword.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (!(txtFieldLength.getText().length() == 0)) {
-                    txtFieldGeneratedPassword.setText(passwordManagerControl.generatePassword(Integer.parseInt(txtFieldLength.getText()), checkBoxIncludeUpper.isSelected(), checkBoxIncludeLower.isSelected(), checkBoxIncludeNumbers.isSelected(), checkBoxIncludeSpecialCharacters.isSelected()));
-                }
-            }
-        });
-        btnSavePassword.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(!(txtFieldLength.getText().length() == 0) && !(txtFieldWebsite.getText().length() == 0) && !(txtFieldUsername.getText().length() == 0) && !(txtFieldGeneratedPassword.getText().length() == 0)) {
-                    passwordManagerControl.savePasswordIntoDatabank(txtFieldWebsite.getText(), txtFieldUsername.getText(), txtFieldGeneratedPassword.getText());
-                } else{
-                    txtUserinformation.setText("You have to fill out all text-fields\nto save a password");
-                }
-            }
-        });
     }
 }
 
